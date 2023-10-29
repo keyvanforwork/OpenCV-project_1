04348ba070395d58e4c3d93bbd059d7a69e5b1a6
keyforwork <keyvanforwork@gmail.com>
2023-10-29 01:00:59 -0700

    Add project files.

---
+++ OpenCV-project_1/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/utils/logger.hpp>
+
+int main() {
+	std::cout << "Open_cv version:" << CV_VERSION;
+	cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
+	// Read image
+	std::string img_path = "directory address";
+	::Mat img = cv::imread(img_path);
+	cv::imshow("Image", img);
+	cv::waitKey(0);
+
+	// Read_video
+	std::string vid_path = "directory address";
+	cv::VideoCapture cap(vid_path);
+	cv::Mat frame;
+	while (true) {
+		cap.read(frame);
+		if (frame.empty())
+			break;
+		cv::imshow("Video", frame);
+		cv::waitKey(20);
+	}
+	cv::waitKey(0);
+
+	// Read_webcam
+	cv::VideoCapture cap(0);
+	cv::Mat frame;
+	while (true) {
+		cap.read(frame);
+		if (frame.empty())
+			break;
+		cv::imshow("Webcam", frame);
+		cv::waitKey(0);
+	}
+	cv::waitKey(0);
+
+	return 0;
+}
