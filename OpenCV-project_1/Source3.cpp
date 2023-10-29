09dc89404d7722a37b6e431bdd2177c2126df804
keyforwork <keyvanforwork@gmail.com>
2023-10-29 06:02:06 -0700

    Added warp prespective image to Source3.cpp file

--- OpenCV-project_1/Source3.cpp
+++ OpenCV-project_1/Source3.cpp
@@ -8,15 +8,19 @@ int main() {
 	std::cout << "Open_cv version:" << CV_VERSION << '\n';
 	cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
 	// Read image
-	// Define a blank image
-	cv::Mat img(512, 512, CV_8UC3, cv::Scalar(120, 59, 43));
-	
-	cv::putText(img, "OpenCV",
-		cv::Point(175, 175), cv::FONT_HERSHEY_SCRIPT_COMPLEX,
-		2, cv::Scalar(200, 140, 100));
-	
+	std::string img_path = "directory address";
+	cv::Mat img = cv::imread(img_path);
+	cv::Mat transform_matrix, img_warp;
+
+	float w = 214, h = 334;
+	cv::Point2f src[4] = { {45, 109}, {302, 5}, {307, 459}, {584, 249} };
+	cv::Point2f dst[4] = { {0.0f, 0.0f}, {w, 0.0f}, {0.0f, h}, {w, h} };
+
+	transform_matrix = cv::getPerspectiveTransform(src, dst);
+	cv::warpPerspective(img, img_warp, transform_matrix, cv::Point(w, h));
 
 	cv::imshow("Image", img);
+	cv::imshow("Warp", img_warp);
 
 	cv::waitKey(0);
 	return 0;
